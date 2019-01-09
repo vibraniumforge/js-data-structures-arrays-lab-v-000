a2cbf3058812bf513350ad2cb2ab878f598eda6d
Greg Morgan <gregm9999@gmail.com>
2019-01-09 00:06:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const drivers =["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
-  drivers.push(name;
+  drivers.push(name);
 }
 
 function destructivelyPrependDriver(name) {
